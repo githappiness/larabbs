5ad4206d2749588a827531ea613e21b6a5b050a4
githappiness <your@example.com>
2024-02-03 02:47:28 +0000

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sourceMaps() // 启用源码映射
+    .version(); // 为编译后的文件名添加哈希值
